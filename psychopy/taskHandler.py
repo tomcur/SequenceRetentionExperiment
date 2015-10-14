1f4c89b53fbb99d453b54445924bf96ec891d63e
DiedeKemper <diede.kemper.18@gmail.com>
2015-10-14 17:09:19 +0200

    color answer panel the same as the others

--- psychopy/taskHandler.py
+++ psychopy/taskHandler.py
@@ -13,6 +13,27 @@ class TaskHandler:
     """
     'Abstract' class implementing a task.
     """
+    colors = {1: "#22b14c", 2: "#ed1c24", 3: "#fff200", 4: "#00a2e8"}
+    color = "#000000"
+    positions = { #normal grid positions
+        1: (-0.225, 0.225), 
+        2: (0.225, 0.225), 
+        3: (0.225, -0.225), 
+        4: (-0.225, -0.225)
+    }
+    answerPositions = { #grid positions of answer grid
+        1: (-0.13, 0.05), 
+        2: (0.13, 0.05), 
+        3: (0.13, -0.21), 
+        4: (-0.13, -0.21)
+    }
+    textPositions = { #positions of key text in answer grid
+        1: (-0.3, 0.05), 
+        2: (0.3, 0.05), 
+        3: (0.3, -0.21), 
+        4: (-0.3, -0.21)
+    }
+    positionKeys = {1: "q", 2: "w", 3: "s", 4: "a"}
     
     def __init__(self, win):
         self.win = win
@@ -52,3 +73,56 @@ class TaskHandler:
             # Present stimulus
             self.present(stimulus)
             core.wait(1)
+
+    def answer(self):
+        """
+        Subject has to recall the sequence.
+        Returns true if the answer was correct, false otherwise.
+        """ 
+        
+        self._showAnswerGrid()
+        alreadyReset = True
+        
+        taskClock = core.Clock()
+        taskClock.reset()
+        
+        event.clearEvents(eventType='keyboard')
+        a = 0
+        while a < len(self.sequence):
+            # Listen for events for keys q, w, s, a
+            theseKeys = event.getKeys(self.positionKeys.values())
+            invKeyMap = {v: k for k, v in self.positionKeys.items()}
+            
+            # Reset answer grid highlight after answer is given 
+            # and specified time has elapsed
+            if not alreadyReset:
+                if taskClock.getTime() >= resetTime:
+                    self._showAnswerGrid()
+                    alreadyReset = True
+            
+            if len(theseKeys) > 0:
+                # Clear keyboard events
+                event.clearEvents(eventType='keyboard')
+                
+                # Get the key that was pressed
+                key = theseKeys[0]
+                
+                # Get the answer that was chosen
+                answer = invKeyMap[key]
+                
+                # Highlight the chosen answer
+                self._showAnswerGrid(answer)
+                
+                # Set time at which to reset the grid to no highlight
+                resetTime = taskClock.getTime() + 1.0
+                alreadyReset = False
+                
+                if self.sequence[a] == answer:
+                    # Correct answer at this position in the sequence
+                    a = a + 1
+                else:
+                    # Incorrect answer
+                    return False
+        core.wait(1)
+        self._showAnswerGrid()
+        return True 
--- psychopy/taskHandlerColor.py
+++ psychopy/taskHandlerColor.py
@@ -33,51 +33,50 @@ class TaskHandlerColor(TaskHandler):
             rect.draw()
             self.win.flip()
            
-    def _showAnswers(self, permutation, highlight=None):
-        """
-        Show the answer grid with given permutation. If a highlight is given,
-        then that color will be highlighted.
-        """
-        p = 0
         
+    def _showAnswerGrid(self,permutation, highlight=None):
+        """
+        Show the answer grid. If a highlight is given,
+        then that entity will be highlighted.
+        """
         #draw text
         txt = "Repeat the sequence by pressing the corresponding keys on your keyboard"
         text = visual.TextStim(win=self.win, text= txt, color='#444444', height=0.05)
         text.setPos((0,0.3))
         text.draw()
         
+        p = 1
+        
         for k in permutation:
-            #draw rectange
-            rect = visual.Rect(self.win, 0.45/self.numOptions, 0.45/self.numOptions)
+            #draw rectangle
+            rect = visual.Rect(self.win, 0.2, 0.2)
             rect.setFillColor(self.colors[k])
-            rect.setPos((-0.5 + float(p) / self.numOptions + 0.1, 0))
+            rect.setPos(self.answerPositions[p])
             rect.setLineWidth(0)
-            if k == highlight:
+            if highlight == k:
                 rect.setLineWidth(10)
                 rect.setLineColor(self.lineColor)
                 
             rect.draw()
             
             #draw corresponding key
-            txt = "[" + str(p+1) + "]"
+            txt = "[" + str(self.positionKeys[k]) + "]"
             key = visual.TextStim(win=self.win,text=txt, color='#444444', height=0.05)
-            key.setPos((-0.5 + float(p) / self.numOptions + 0.1, -0.1))
+            key.setPos(self.textPositions[k])
             key.draw()
             
             p = p + 1
             
         self.win.flip()
-        
-           
+       
     def answer(self):
         """
         Subject has to recall the sequence.
         Returns true if the answer was correct, false otherwise.
         """ 
-        
         permutation = numpy.random.permutation(self.colors.keys())
         
-        self._showAnswers(permutation)
+        self._showAnswerGrid(permutation)
         alreadyReset = True
         
         taskClock = core.Clock()
@@ -86,14 +85,15 @@ class TaskHandlerColor(TaskHandler):
         event.clearEvents(eventType='keyboard')
         a = 0
         while a < len(self.sequence):
-            # Listen for events for keys 1, 2, 3 and 4
-            theseKeys = event.getKeys(['1', '2', '3', '4'])
+            # Listen for events for keys q, w, s, a
+            theseKeys = event.getKeys(self.positionKeys.values())
+            invKeyMap = {v: k for k, v in self.positionKeys.items()}
             
             # Reset answer grid highlight after answer is given 
             # and specified time has elapsed
             if not alreadyReset:
                 if taskClock.getTime() >= resetTime:
-                    self._showAnswers(permutation)
+                    self._showAnswerGrid(permutation)
                     alreadyReset = True
             
             if len(theseKeys) > 0:
@@ -101,14 +101,15 @@ class TaskHandlerColor(TaskHandler):
                 event.clearEvents(eventType='keyboard')
                 
                 # Get the key that was pressed
-                key = int(theseKeys[0])-1    
+                key = theseKeys[0]
                 
-                # Get which answer is associated with this key
-                answer = permutation[key]
+                # Get the answer that was chosen
+                index = invKeyMap[key]
+                answer = permutation[(index-1)]
                 
-                # Highlight the chosen color
-                self._showAnswers(permutation, answer)
-            
+                # Highlight the chosen answer
+                self._showAnswerGrid(permutation, answer)
+                
                 # Set time at which to reset the grid to no highlight
                 resetTime = taskClock.getTime() + 1.0
                 alreadyReset = False
@@ -120,5 +121,6 @@ class TaskHandlerColor(TaskHandler):
                     # Incorrect answer
                     return False
         core.wait(1)
-        self._showAnswers(permutation)
+        self._showAnswerGrid(permutation)
         return True 
+    
