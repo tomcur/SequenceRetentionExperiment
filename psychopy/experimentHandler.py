658b74109b637623ad81a76370fa2962b896924f
DiedeKemper <diede.kemper.18@gmail.com>
2015-10-15 14:31:18 +0200

    Data stored in csv format. Order of conditions is counterbalanced.

--- psychopy/experimentHandler.py
+++ psychopy/experimentHandler.py
@@ -6,7 +6,7 @@ Module that provides experiment handling. This controls the flow of the experime
 """
 
 from psychopy import visual, core, data, event, logging, sound, gui
-from numpy import random
+from itertools import *
 from showText import *
 from trialHandler import *
 from taskHandlerColor import *
@@ -32,7 +32,7 @@ class ExperimentHandler:
         
         # Experimenter info
         expName = self.showText.askQuestionUntilAnswered(u"Give experimenter name + press [enter]:");
-        subjNumber = self.showText.askQuestionUntilAnswered(u"Give subject number + press [enter]:");
+        subjNr = self.showText.askQuestionUntilAnswered(u"Give subject number + press [enter]:");
         
         # Welcome message 
         self.showText.showUntilKeyPressed(u"Welcome! Indicate you have read the text by pressing [space].") 
@@ -41,24 +41,23 @@ class ExperimentHandler:
         
         self.showText.showUntilKeyPressed(u"The experiment will now begin.") 
         
-        # TODO: counterbalance instead of randomize
-        permutation = numpy.random.permutation(['color', 'position', 'colorPosition']);
+        # decide order of conditions
+        order = self.giveOrder(subjNr)
+        orderStr = order[0] + "-" + order[1] + "-" + order[2]
         
         # Txt file to save data in
-        file = open("data" + expName + str(subjNumber) + ".txt", "w")
-        file.write("experimenter: " + expName + "\n")
-        file.write("subject: " + str(subjNumber) + "\n")
-        file.write("order: " + permutation[0] + ", " + permutation[1] + ", " + permutation[2]+"\n")
+        file = open("data" + expName + str(subjNr) + ".csv", "w")
+        file.write("Nr,exp,order,cond,trial,score\n")
         
         nrOfTasks = 2 #nr of tasks per condition
         c = 1 #subject is at the c'th condition
         
         # walk through conditions
-        for p in permutation:
-            if p == 'color':
+        for p in order:
+            if p == 'c':
                 trial = TrialHandler(self.win, nrOfTasks, c, TaskHandlerColor(self.win))
                 scores = trial.run()
-            elif p == 'position':
+            elif p == 'p':
                 trial = TrialHandler(self.win, nrOfTasks, c, TaskHandlerPosition(self.win))
                 scores = trial.run()
             else:
@@ -67,7 +66,7 @@ class ExperimentHandler:
             #next condition
             c = c + 1 
             # Save data in each condition
-            file.write(self.toDataString(p,scores))
+            file.write(self.toDataString(subjNr,expName, orderStr, p, scores))
         
         file.close()
         
@@ -111,17 +110,26 @@ class ExperimentHandler:
         colorPositionTask = TaskHandlerColorPosition(self.win)
         colorPositionTask.run()
         
-    def toDataString(self, condition, scores):
+    def toDataString(self, subjNr, expName, order,condition, scores):
         """
         Creates data string from the scores from the given condition
         condition = string containing condition name
         scores = list with scores of all tasks within this condition
         """
-        
+        trial = 1
         ff = ""
         for s in scores:
-            ff = ff + str(s) + ","
+            ff = ff + subjNr + "," + expName + "," + order + "," + condition + "," + str(trial) + "," + str(s) + "\n"
+            trial = trial + 1
             
-        return condition + ":" + ff + "\n"
+        return ff
+        
+    def giveOrder(self,subjNr):
+        """
+        Decides in which order the conditions will appear for this subject. A counterbalansing
+        method is used based on the subject number.
+        """
+        orders = list(permutations(["c","p","cp"]))
+        return orders[int(float(subjNr))-1]
         
 
